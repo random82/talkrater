8733d7c8eecfe50c7d7e04bd5f213a1631f04013
Dominik Raniszewski <dominik.raniszewski@gmail.com>
2020-01-02 11:49:22 +1100

    New app created

---
+++ talkrater/android/app/src/main/kotlin/com/domb2b/talkrater/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.domb2b.talkrater
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
