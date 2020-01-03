423af57653b0e577a848ddd25c3e7a7cbf2570a6
Dominik Raniszewski <dominik.raniszewski@gmail.com>
2020-01-03 11:42:05 +1100

    Refactoring: TalkCardDetailsSection extracted

--- talkrater/lib/talkcard.dart
+++ talkrater/lib/talkcard.dart
@@ -1,6 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+class TalkCardDetailsSection extends StatelessWidget {
+
+  final String presenter;
+
+  final DateTime talkTime;
+
+  final String location;
+
+  Duration get _startingIn => talkTime.difference(DateTime.now());
+
+  String get formattedTalkTime => DateFormat.EEEE().add_jm().format(talkTime);
+
+  String get formattedStartingIn => _formatDuration(_startingIn);
+
+  String _formatDuration(Duration duration) {
+    if(duration < Duration.zero) {
+      return 'Started already';
+    }
+
+    if(duration.inDays > 0){
+      return 'Starting in ${duration.inDays} days';
+    }
+
+    return 'Starting in ${duration.inHours}:${duration.inMinutes}';
+  }
+
+  TalkCardDetailsSection({this.presenter, this.talkTime, this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 5),
+                      child:Text(
+                        this.presenter,
+                        style: TextStyle (
+                          fontSize: 20 
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 5),
+                      child:Text(
+                        this.formattedTalkTime,
+                        style: TextStyle (
+                          fontSize: 20 
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]
+          )
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(
+                        Icons.map
+                      ),
+                    ),
+                    Text(
+                      this.location,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(
+                        Icons.watch_later
+                      ),
+                    ),
+                    Text(
+                      this.formattedStartingIn,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic
+                      ),
+                    ),
+                  ],
+                )
+              ]
+          )
+        )
+      ],
+    );
+  }
+}
+
 class TalkCardTitleSection extends StatelessWidget {
 
   final String title;
@@ -81,24 +191,6 @@ class TalkCard extends StatelessWidget {
 
   final DateTime talkTime;
 
-  Duration get _startingIn => talkTime.difference(DateTime.now());
-
-  String get formattedTalkTime => DateFormat.EEEE().add_jm().format(talkTime);
-
-  String get formattedStartingIn => _formatDuration(_startingIn);
-
-  String _formatDuration(Duration duration) {
-    if(duration < Duration.zero) {
-      return 'Started already';
-    }
-
-    if(duration.inDays > 0){
-      return 'Starting in ${duration.inDays} days';
-    }
-
-    return 'Starting in ${duration.inHours}:${duration.inMinutes}';
-  }
-
   final String location;
 
   TalkCard({this.title, this.talkAbstract, this.presenter, this.talkTime, this.location});
@@ -106,116 +198,44 @@ class TalkCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Card(
-              child:Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: TalkCardTitleSection(title: this.title)
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                        this.talkAbstract,
-                        maxLines: 5,
-                        softWrap: true,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
-                            children: <Widget>[
-                              Align(
-                                alignment: Alignment.centerLeft,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 5),
-                                      child:Text(
-                                        this.presenter,
-                                        style: TextStyle (
-                                          fontSize: 20 
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 5),
-                                      child:Text(
-                                        this.formattedTalkTime,
-                                        style: TextStyle (
-                                          fontSize: 20 
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ]
-                          )
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: EdgeInsets.only(right: 10),
-                                      child: Icon(
-                                        Icons.map
-                                      ),
-                                    ),
-                                    Text(
-                                      this.location,
-                                      style: TextStyle(
-                                        fontStyle: FontStyle.italic
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: EdgeInsets.only(right: 10),
-                                      child: Icon(
-                                        Icons.watch_later
-                                      ),
-                                    ),
-                                    Text(
-                                      this.formattedStartingIn,
-                                      style: TextStyle(
-                                        fontStyle: FontStyle.italic
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ]
-                          )
-                        )
-                      ],
-                    )
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20,0,20,20),
-                    child: Row(
-                      children: <Widget>[
-                        Text("Rate:"),
-                        Icon(Icons.star_border),
-                        Icon(Icons.star_border),
-                        Icon(Icons.star_border),
-                        Icon(Icons.star_border),
-                        Icon(Icons.star_border),
-                      ],
-                    )
-                  ),
-                ]
-              )
-            );
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TalkCardTitleSection(title: this.title)
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+                this.talkAbstract,
+                maxLines: 5,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+              ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: TalkCardDetailsSection(
+              location: this.location,
+              presenter: this.presenter,
+              talkTime: this.talkTime,
+            )
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20,0,20,20),
+            child: Row(
+              children: <Widget>[
+                Text("Rate:"),
+                Icon(Icons.star_border),
+                Icon(Icons.star_border),
+                Icon(Icons.star_border),
+                Icon(Icons.star_border),
+                Icon(Icons.star_border),
+              ],
+            )
+          )
+        ]
+      )
+    );
   }
 }
