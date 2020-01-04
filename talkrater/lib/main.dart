521f064205d6457462feab15fe8b402b5a9170bd
Dominik Raniszewski <dominik.raniszewski@gmail.com>
2020-01-04 11:10:13 +1100

    Changed Column to ListView in the talk list UI

--- talkrater/lib/main.dart
+++ talkrater/lib/main.dart
@@ -94,43 +94,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Center(
           child: Text(widget.title),
         )
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: talks.map((talk){
-            return TalkCard(
-              location: talk.location,
-              presenter: talk.presenter,
-              tags: talk.tags,
-              talkAbstract: talk.talkAbstract,
-              talkTime: talk.talkTime,
-              title: talk.title,
+      body: ListView.builder(
+        itemCount: talks.length,
+        itemBuilder: (context, it) {
+          return TalkCard(
+              location: talks[it].location,
+              presenter: talks[it].presenter,
+              tags: talks[it].tags,
+              talkAbstract: talks[it].talkAbstract,
+              talkTime: talks[it].talkTime,
+              title: talks[it].title,
             );
-          }).toList()
+          }
         ),
-      ),
     );
   }
 }
